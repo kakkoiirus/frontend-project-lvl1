5ef28806bbd5f351c6860ae86c0e630e2b883a67
Valentin <bagadin@inbox.ru>
2020-12-12 13:28:01 +0500

    Step 9: Brain-Prime game

---
+++ bin/brain-prime.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+import startGame from '../src/index.js';
+import primeGame from '../src/games/prime-game.js';
+
+startGame(primeGame);
---
+++ src/games/prime-game.js
@@ -0,0 +1,22 @@
+import { getRandomInRange } from '../utils.js';
+
+const MAX_NUMBER = 10;
+
+const isPrime = (num) => {
+  for (let i = 2; i < num; i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return num > 1;
+};
+
+export default {
+  description: 'Answer "yes" if given number is prime. Otherwise answer "no".',
+  play() {
+    const question = getRandomInRange(2, MAX_NUMBER);
+    const expectedAnswer = isPrime(question) ? 'yes' : 'no';
+
+    return { expectedAnswer, question };
+  },
+};
